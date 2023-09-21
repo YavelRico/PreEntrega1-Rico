2ff96bd76df046d48651bc7a440a8ef3835eb988
yavel <yavelwork@gmail.com>
2023-09-21 03:12:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from './assets/components/NavBar';
+import ItemListContainer from './assets/components/ItemListContainer';
+
+function App() {
+  return (
+   <>
+    <NavBar />
+    <ItemListContainer />
+   </>
+  );
+}
+
+export default App
---
+++ src/assets/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import CartWidget from "./CartWidget";
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="#">Logo</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Productos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Servicios</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contáctate</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Acerca de Nosotros</a>
+                        </li>
+                        <li className="nav-item">
+                            <CartWidget />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
